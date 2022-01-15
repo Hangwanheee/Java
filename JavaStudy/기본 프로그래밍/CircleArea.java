9c3e69fe7a878adc49f8760ded8c846c9d0975cb
Hangwanheee <han355803@naver.com>
2022-01-15 18:11:15 +0900

    HomeWork(in 20200115)

--- JavaStudy/기본 프로그래밍/CircleArea.java
+++ JavaStudy/기본 프로그래밍/CircleArea.java
@@ -4,7 +4,8 @@ public class CircleArea {
 		double radius = 10.2;
 		double circleArea = radius*radius*PI;
 				
-		
+		System.out.print("반지름 " + radius + ", ");
+		System.out.print("원의 면적 = " + circleArea);
 	}
 
 }
